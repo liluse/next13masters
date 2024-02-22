483133a14c9b1d525b267e219eb47220f6b9691b
liluse <pietrzaknatalia2598@gmail.com>
2024-02-22 23:26:26 +0100

    extract product page & add styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,3 @@
-import { ProductList } from "@/components/organisms/ProductsList";
-import { type ProductType } from "@/components/types";
-
-const products: ProductType[] = [
-	{
-		id: 1,
-		name: "Róże niebieskie",
-		price: 100,
-		image: "/blue.jpeg",
-	},
-	{
-		id: 2,
-		name: "Róże czerwone",
-		price: 200,
-		image: "/red.jpeg",
-	},
-	{
-		id: 3,
-		name: "Róże herbaciane",
-		price: 300,
-		image: "/herb.jpeg",
-	},
-	{
-		id: 4,
-		name: "Róże różowe",
-		price: 400,
-		image: "/pink.jpeg",
-	},
-];
-
-export default function Home() {
-	return (
-		<section className="mx-auto">
-			<ProductList products={products} />
-		</section>
-	);
+export default function HomePage() {
+	return <div>home</div>;
 }
--- src/components/atoms/ProductDescription.tsx
+++ src/components/atoms/ProductDescription.tsx
@@ -1,13 +1,26 @@
 import { type ProductDescriptionType } from "../types";
+import { formatMoney } from "@/utils";
 
 export const ProductDescription = ({
 	name,
 	price,
 }: ProductDescriptionType) => {
 	return (
-		<>
-			<h4 className="text-2xl uppercase">{name}</h4>
-			<p className="text-right text-sm font-bold">{price}</p>
-		</>
+		<div className="mt-2 flex justify-between">
+			<div>
+				<h3 className="text-sm font-semibold text-gray-700">
+					{name}
+				</h3>
+
+				<p className="text-sm text-gray-500">
+					<span className="sr-only">Kategoria: </span> Kwiaty
+				</p>
+			</div>
+
+			<p className="text-sm font-medium text-gray-900">
+				<span className="sr-only">Cena:</span>
+				{formatMoney(price)}
+			</p>
+		</div>
 	);
 };
